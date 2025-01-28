7f0ef134b6748b2727e80e7334485d00860b933f
md ehtesham <ehteshammd089@gmail.com>
2025-01-28 09:59:41 +0530

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -39,17 +39,67 @@ class ModelTrainer:
                 "KNeighbors": KNeighborsRegressor(),
                 "AdaBoost": AdaBoostRegressor(),
                 "GradientBoost": GradientBoostingRegressor(),
-                "XGB": XGBRegressor(),
+                "XGB": XGBRegressor(use_label_encoder=False, eval_metric="mlogloss"),
                 "CatBoost": CatBoostRegressor(verbose=False)
             }
-            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params = {
+            "RandomForest": {
+                # 'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                # 'max_features': ['sqrt', 'log2', None],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "DecisionTree": {
+                'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                # 'splitter': ['best', 'random'],
+                # 'max_features': ['sqrt', 'log2'],
+            },
+            "LinearRegression": {},
+            "KNeighbors": {},
+            "AdaBoost": {
+                'learning_rate': [.1, .01, 0.5, .001],
+                # 'loss': ['linear', 'square', 'exponential'],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "GradientBoost": {
+                # 'loss': ['squared_error', 'huber', 'absolute_error', 'quantile'],
+                'learning_rate': [.1, .01, .05, .001],
+                'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                # 'criterion': ['squared_error', 'friedman_mse'],
+                # 'max_features': ['auto', 'sqrt', 'log2'],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "XGB": {
+                'learning_rate': [.1, .01, .05, .001],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "CatBoost": {
+                'depth': [6, 8, 10],
+                'learning_rate': [0.01, 0.05, 0.1],
+                'iterations': [30, 50, 100]
+            } # keeping null for catboost to solve the incompatiblily issue
+            # "CatBoost": {
+            #     'depth': [6, 8, 10],
+            #     'learning_rate': [0.01, 0.05, 0.1],
+            #     'iterations': [30, 50, 100]
+            # }
+            }
+            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train,X_test=X_test,y_test=y_test,models=models,params=params)
+            values = [val for val in model_report.values()]
+            best_model_score = max(values)
+            
 
-            best_model_score = max(sorted(model_report.values()))
+
+
+            # best_model_score = max(sorted(model_report.values()))
+            # best_model_score = max([model['test_score'] for model in model_report.values()])
+            # best_model_name = max(model_report, key=model_report.get)  # This will give you the model name with the highest score
+            # best_model_score = model_report[best_model_name]  # This gives you the best score
+
 
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
-
             best_model = models[best_model_name]
-
+            logging.info(f"Model report: {model_report}")
+            logging.info(f"{best_model_name} is the best model with score: {best_model_score} and type:{type(best_model),{type(best_model_score)},{type(best_model_name)}}")
             if best_model_score < 0.6:
                 raise CustomException("no best model found!",sys)
             logging.info(f"Best model found: {best_model_name} with score: {best_model_score}")
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,9 @@ import pandas as pd
 import dill 
 import os 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+
 
 def save_object(file_path,obj):
     try:
@@ -20,22 +23,95 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
 
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+# def evaluate_models(X_train,y_train,X_test,y_test,models,params):
+#     try:
+#         report = {}
+#         for i in range(len(list(models))):
+#             model_name = list(models.keys())[i]
+#             model = list(models.values())[i]
+#             para = params[model_name]
+
+#             if model_name == "CatBoost":  # Skip GridSearchCV for CatBoost
+#                 logging.info(f"Using CatBoost's internal grid search for {model_name}")
+#                 model.grid_search(para, X=X_train, y=y_train)
+#                 y_test_pred = model.predict(X_test)
+#                 report[model_name] = r2_score(y_test, y_test_pred)
+#                 continue
+
+
+            
+#             gs = GridSearchCV(model,para,cv=3)
+#             gs.fit(X_train,y_train)
+
+#             # best_model = gs.best_estimator_  # Use the best estimator
+
+#             model.set_params(**gs.best_params_)
+#             model.fit(X_train,y_train)
+            
+#             # model.fit(X_train,y_train)
+            
+            
+#             y_train_pred = model.predict(X_train)
+#             y_test_pred = model.predict(X_test)
+
+#             train_model_score = r2_score(y_train,y_train_pred)
+#             test_model_score = r2_score(y_test,y_test_pred)
+            
+#             report[list(models.keys())[i]] = test_model_score
+
+#         return report
+
+#     except Exception as e:
+#         raise CustomException(e,sys)
+    
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            model.fit(X_train,y_train)
 
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+        for model_name, model in models.items():
+            try:
+                # Get the hyperparameters for the model
+                param_grid = params.get(model_name, {})
 
-            train_model_score = r2_score(y_train,y_train_pred)
-            test_model_score = r2_score(y_test,y_test_pred)
-            
-            report[list(models.keys())[i]] = test_model_score
+                # If the model has a param grid, we perform GridSearchCV
+                if param_grid:
+                    gs = GridSearchCV(model, param_grid, cv=3, verbose=1, n_jobs=-1)
+                    gs.fit(X_train, y_train)
+                    model.set_params(**gs.best_params_)  # Set the best parameters found
+                    logging.info(f"Best params for {model_name}: {gs.best_params_}")
+                else:
+                    logging.info(f"No hyperparameters to tune for {model_name}")
 
+                # Fit the model on the training data
+                model.fit(X_train, y_train)
+
+                # Predict on both train and test data
+                y_train_pred = model.predict(X_train)
+                y_test_pred = model.predict(X_test)
+
+                # Calculate R-squared scores
+                train_model_score = r2_score(y_train, y_train_pred)
+                test_model_score = r2_score(y_test, y_test_pred)
+
+                # Store the results in the report
+                # report[model_name] = {
+                #     "train_score": train_model_score,
+                #     "test_score": test_model_score
+                # }
+                report[model_name] = test_model_score
+                logging.info(f"Model: {model_name}, Train Score: {train_model_score}, Test Score: {test_model_score}")
+
+
+            except Exception as e:
+                logging.error(f"Error occurred while training model {model_name}: {e}")
+                # report[model_name] = {"train_score": None, "test_score": None}
+                continue
+
+        # Return the final report with all models' performance
         return report
 
     except Exception as e:
-        raise CustomException(e,sys)
+        logging.error(f"An error occurred in the evaluate_models function: {e}")
+        raise CustomException(e, sys)
